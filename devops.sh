e95cef2088bb799f7bd437201331b5349cfcfb5e
dileepchinni <dileepu95@gmail.com>
2022-02-28 23:20:53 +0530

    this is all about git hub

---
+++ devops.sh
@@ -0,0 +1,5 @@
+git hub is a centraized version control system
+where as git hub is a local repositry system
+git and git hub both are combined to get a repository to store our source code
+and its a combination of git and github
+it is a key to maintain our source code in a proper way
